9abb6be2fb14010895ccde9cc86b01289cdf7338
sagar.bhosale <sagar.bhosale@tyfone.com>
2017-11-15 11:58:42 +0530

    made priority queue with heap more generic

--- priorityqueueHeap/priorityqueueHeap/Program.cs
+++ priorityqueueHeap/priorityqueueHeap/Program.cs
@@ -6,21 +6,213 @@ using System.Threading.Tasks;
 
 namespace priorityqueueHeap
 {
+
+    //class Program
+    //{
+
+    //    static int[] arr = null;
+
+    //    static void Main(string[] args)
+    //    {
+
+    //        arr = new int[] { 1, 4, 3, 7, 8, 9, 10, 2, 5 };
+
+    //        //heapMinsort(arr);
+
+    //        buildminHeap(arr);
+
+    //        Console.WriteLine("Extracted minimum is " + extractMinimum());
+
+    //        Console.WriteLine("Extracted minimum is " + extractMinimum());
+
+    //        // heapMinsort(arr);
+
+    //        decreaseValue(5, 1);
+    //        Console.ReadLine();
+
+    //    }
+
+
+
+    //    static void heapMinsort(int[] arr)
+    //    {
+
+    //        int size = arr.Count() - 1;
+
+    //        buildminHeap(arr);
+
+    //        for (int i = size; i >= 1; i--)
+    //        {
+
+    //            swap(arr, 0, i);
+
+    //            size--;
+
+    //            minheapify(arr, 0, size);
+
+    //        }
+
+    //    }
+
+
+
+
+
+    //    static void buildminHeap(int[] arr)
+    //    {
+
+    //        for (int i = (arr.Count() - 1) / 2; i >= 0; i--)
+    //        {
+
+    //            minheapify(arr, i, arr.Count() - 1);
+
+    //        }
+
+    //    }
+
+    //    static void minheapify(int[] arr, int i, int n)
+    //    {
+
+    //        int left = 2 * i + 1;
+
+    //        int right = (2 * i) + 2;
+
+    //        int smallest = -1;
+
+
+
+    //        if (left <= n && arr[left] < arr[i])
+    //        {
+
+    //            smallest = left;
+
+    //        }
+
+    //        else
+    //        {
+
+    //            smallest = i;
+
+    //        }
+
+    //        if (right <= n && arr[right] < arr[smallest])
+    //        {
+
+    //            smallest = right;
+
+    //        }
+
+    //        if (smallest != i && smallest != -1)
+    //        {
+
+    //            swap(arr, i, smallest);
+
+    //            minheapify(arr, smallest, n);
+
+    //        }
+
+    //    }
+
+    //    static void swap(int[] arr, int a, int b)
+    //    {
+
+    //        int temp = arr[a];
+
+    //        arr[a] = arr[b];
+
+    //        arr[b] = temp;
+
+    //    }
+
+
+
+    //    static int minimum()
+    //    {
+
+    //        return arr[0];
+
+    //    }
+
+
+
+    //    static int extractMinimum()
+    //    {
+
+    //        int minimum = int.MinValue;
+
+    //        minimum = arr[0];
+
+    //        arr[0] = arr.Last();
+
+    //        Array.Resize(ref arr, arr.Count() - 1);
+
+    //        minheapify(arr, 0, arr.Count() - 1);
+
+    //        return minimum;
+
+    //    }
+
+
+
+    //    static void decreaseValue(int i, int val)
+    //    {
+
+    //        if (val < arr[i])
+    //        {
+
+    //            arr[i] = val;
+
+    //            while (i > 0 && arr[i / 2] > arr[i])
+    //            {
+
+    //                swap(arr, i / 2, i);
+
+    //                i = i / 2;
+
+    //            }
+
+    //        }
+
+    //    }
+
+    //}
+
+    public class Item
+    {
+        public char vertex;
+        public int key;
+
+        public Item(char vertex, int key)
+        {
+            this.vertex = vertex;
+            this.key = key;
+        }
+    }
+
     class Program
     {
-        static int[] arr = null;
+        static Item[] arr = null;
         static void Main(string[] args)
         {
-            arr = new int[] { 1, 4, 3, 7, 8, 9, 10, 2, 5 };
+            var input = new int[] { 1, 4, 3, 7, 8, 9, 10, 2, 5 };
+            //var input = new char[] { '1', '4', '3', '7', '8', '9', '2', '5' };
+            arr = new Item[input.Count()];
+            for (int i = 0; i < arr.Count();i++ )
+            {
+                arr[i] = new Item('A', input[i]);
+            }
+                //heapMinsort(arr);
+                buildminHeap(arr);
+            Console.WriteLine("Extracted minimum is " + extractMinimum());
+            Console.WriteLine("Extracted minimum is " + extractMinimum());
             //heapMinsort(arr);
-            buildminHeap(arr);
-            Console.WriteLine("Extracted minimum is " + extractMinimum());
-            Console.WriteLine("Extracted minimum is " + extractMinimum());
-           // heapMinsort(arr);
-            decreaseValue(5, 1);
+            arr[5].vertex = 'F';
+            decreaseValue(arr[5], 1);
+            heapMinsort(arr);
+            Console.ReadLine();
         }
 
-        static void heapMinsort(int[] arr)
+        static void heapMinsort(Item[] arr)
         {
             int size = arr.Count() - 1;
             buildminHeap(arr);
@@ -33,20 +225,20 @@ namespace priorityqueueHeap
         }
 
       
-        static void buildminHeap(int[] arr)
+        static void buildminHeap(Item[] arr)
         {
             for (int i = (arr.Count() - 1) / 2; i >= 0; i--)
             {
                 minheapify(arr, i, arr.Count() - 1);
             }
         }
-        static void minheapify(int[] arr, int i, int n)
+        static void minheapify(Item[] arr, int i, int n)
         {
             int left = 2 * i + 1;
             int right = (2 * i) + 2;
             int smallest = -1;
 
-            if (left <= n && arr[left] < arr[i])
+            if (left <= n && arr[left].key < arr[i].key)
             {
                 smallest = left;
             }
@@ -54,7 +246,7 @@ namespace priorityqueueHeap
             {
                 smallest = i;
             }
-            if (right <= n && arr[right] < arr[smallest])
+            if (right <= n && arr[right].key < arr[smallest].key)
             {
                 smallest = right;
             }
@@ -64,21 +256,21 @@ namespace priorityqueueHeap
                 minheapify(arr, smallest, n);
             }
         }
-        static void swap(int[] arr, int a, int b)
+        static void swap(Item[] arr, int a, int b)
         {
-            int temp = arr[a];
+            var temp = arr[a];
             arr[a] = arr[b];
             arr[b] = temp;
         }
 
-        static int minimum()
+        static Item minimum()
         {
             return arr[0];
         }
 
-        static int extractMinimum()
+        static Item extractMinimum()
         {
-            int minimum = int.MinValue;
+            Item minimum = null;
             minimum = arr[0];
             arr[0] = arr.Last();
             Array.Resize(ref arr, arr.Count() - 1);
@@ -86,17 +278,33 @@ namespace priorityqueueHeap
             return minimum;
         }
 
-        static void decreaseValue(int i, int val)
+        static void decreaseValue(Item item, int val)
         {
-            if(val<arr[i])
+            int i = findIndex(item);
+            if (i != -1)
             {
-                arr[i] = val;
-                while(i>0 && arr[i/2]>arr[i])
+                if (val < arr[i].key)
                 {
-                    swap(arr, i / 2, i);
-                    i = i / 2;
+                    arr[i].key = val;
+                    while (i > 0 && arr[i / 2].key > arr[i].key)
+                    {
+                        swap(arr, i / 2, i);
+                        i = i / 2;
+                    }
                 }
             }
+        }
+
+        static int findIndex(Item item)
+        {
+            for(int i=0; i<arr.Count();i++)
+            {
+                if(arr[i].vertex==item.vertex)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
